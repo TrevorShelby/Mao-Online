eb86d0e8ae42dbcd691af5e68947834074d6e52b
TrevorShelby <trvrshelby@gmail.com>
2019-08-07 11:07:50 -0500

    Added test directory

--- server/server.js
+++ server/server.js
@@ -4,5 +4,8 @@ const WebSocket = require('ws')
 const server = http.createServer()
 
 const wsServer = new WebSocket.Server({server})
+wsServer.on('connection', (req, res) => {
+
+})
 
 server.listen(1258)

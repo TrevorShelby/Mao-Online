8b8ec89c4e42f313d10db09566a024e99599eea5
TrevorShelby <trvrshelby@gmail.com>
2019-08-21 07:29:29 -0500

    added starter code for acceptAccusation during lastChance

--- server/utility/actions/acceptAccusation.js
+++ server/utility/actions/acceptAccusation.js
@@ -4,14 +4,28 @@ const { sendEvent_ } = require('../sendMessage.js')
 
 function acceptAccusation_(game, actionPools, accusedSeat) {
 	function acceptAccusation() {
-		actionPools.forEach( (actionPool) => {
-			actionPool.changeActivityByTags(
-				(tags) => { return tags.includes('play') }
-			)
-		})
+		if(game.round.mode == 'play') {
+			cancelAccusationDuringPlay()
+		}
+		//second condition should always be true if the game.round.mode is lastChance. (don't
+		//remove though)
+		else if(
+			game.round.mode == 'lastChance'
+			&& game.round.accusation.accused == game.round.winner
+		) {
+			//TODO: add resume timer or whatever and then accept accusation. use same event.
+		}
 
-		game.round.accusation = undefined
-		sendEvent_(game, game.round.seating)('accusationAccepted')
+		function acceptAccusationDuringPlay() {
+			actionPools.forEach( (actionPool) => {
+				actionPool.changeActivityByTags(
+					(tags) => { return tags.includes('play') }
+				)
+			})
+
+			game.round.accusation = undefined
+			sendEvent_(game, game.round.seating)('accusationAccepted')
+		}
 	}
 	return acceptAccusation
 }

0b09fa963ec36ced98060e843d08fb7d67a850db
TrevorShelby <trvrshelby@gmail.com>
2019-08-21 16:21:55 -0500

    Added test server

---
+++ server/test/testServer.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws')
+
+const { createNewGame, createGameActionPools } = require('../utility/newGame.js')
+
+
+
+const connections = []
+const numPlayersToStart = 4
+const wsServer = new WebSocket.Server({port: 1258})
+wsServer.on('connection', onConnectionDuringLobby)
+
+function onConnectionDuringLobby(conn) {
+	connections.push(conn)
+	if(connections.length == numPlayersToStart) {
+		wsServer.off('connection', onConnectionDuringLobby)
+		const game = createNewGame(0, connections)
+		createGameActionPools(game)
+	}
+}

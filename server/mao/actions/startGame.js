e92aa51d60fc6e17abd9bbc63c85aaf0baef7460
TrevorShelby <trvrshelby@gmail.com>
2019-08-23 17:22:30 -0500

    small comment and added test script in package.json

--- server/mao/actions/startGame.js
+++ server/mao/actions/startGame.js
@@ -8,6 +8,8 @@ function startGame_(table, sendEvent, starterID) {
 		if(starterID != table.hostID) { return }
 
 		table.mode = 'game'
+		//Passing sendEvent here is find. startGame could just as well have used the createNewGame
+		//code. It doesn't though, because that would make it a bit messier.
 		table.game = createNewGame(table.playerConnections, sendEvent)
 		sendEvent(table.game.playerIDs, 'gameStarted')
 		sendEvent(table.game.round.seating, 'roundStarted')

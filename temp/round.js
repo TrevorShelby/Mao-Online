c5167db74f5772c3dc50ce0ba1c4a2dd7167e98d
TrevorShelby <trvrshelby@gmail.com>
2019-08-29 10:42:45 -0500

    Removed unnecessary files from repo

--- temp/round.js
+++
@@ -1,8 +0,0 @@
-//TODO: Rework so that there can be multiple rounds (necessary for multiple tables).
-const rounds = {
-	hands: [],
-	piles: [{owner: undefined, cards: []}]
-}
-
-
-module.exports = round
